7351dd6f25f363fbad27f0293d67daf3f32d4151
atlassubbed <atlassubbed@gmail.com>
2019-03-03 20:11:16 -0500

    Adds boilerplate src/test files, etc. Commit made by atlas-npm-init, an npm starter-app generator.

---
+++ README.md
@@ -0,0 +1,7 @@
+# atlas-relax
+
+A minimal, powerful declarative VDOM and reactive programming framework.
+
+[![Travis](https://img.shields.io/travis/[username]/[repo].svg)](https://travis-ci.org/[username]/[repo])
+
+---
---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+const { describe, it } = require("mocha")
+const { expect } = require("chai")
+
+// default placeholder test
+describe("package", function(){
+  it("exists", function(){
+    expect(true).to.be.true;
+  })
+})
